34ec1d79254ae92e72948cf2607a335dc5e03639
Chang <xiangxiang3451@gmail.com>
2023-11-16 02:00:17 +0700

    change1

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 function appendValue(value) {
     var inputValue = document.getElementById('textInput');
+    var displayDiv = document.getElementById('text2');
+    // var currentValue = inputValue.textContent;
+    var lastChar = inputValue.textContent[inputValue.textContent.length - 1];
 
-    inputValue.textContent+=value;
+    if (((lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/')) && (value === '+' || value === '-' || value === "*" || value === "/")) {
+        inputValue.textContent = inputValue.textContent.slice(0, -1) + value;
+    } else {
+        inputValue.textContent += value;
+    }
 
-    var currentValue = inputValue.textContent;
-    var grayedOutValue = currentValue.replace(/(.*)([+\-*/])/g, '<span class="gray">$1</span>$2');
-    
-    var displayDiv=document.getElementById('text2');
+    var grayedOutValue = inputValue.textContent.replace(/(.*)([+\-*/])/g, '<span class="gray">$1</span>$2');
 
-    displayDiv.innerHTML=grayedOutValue;
-    
+    displayDiv.innerHTML = grayedOutValue;
+
 }
 
 function clearNum() {
     document.getElementById('text2').textContent = "";
     document.getElementById('textInput').textContent = "";
-    
+
 }
 
 function calculate() {
@@ -25,15 +29,20 @@ function calculate() {
 
     var result2 = parseFloat(result.toFixed(10));
 
-    var input = document.getElementById("text2").textContent=result2;
-    document.getElementById('textInput').textContent=result2;
-
+    var input = document.getElementById("text2").textContent = result2;
+    document.getElementById('textInput').textContent = result2;
 }
 
 function back() {
-    var inputValue = document.getElementById('text2');
+    var inputValue = document.getElementById('textInput');
     var text2 = document.getElementById('text2');
 
     inputValue.textContent = inputValue.textContent.slice(0, -1);
-    text2.textContent = inputValue.textContent.slice(0, -1);
+    text2.textContent = inputValue.textContent;
+}
+function appendDot() {
+    if (document.getElementById('text2').textContent[document.getElementById('text2').textContent.length - 1] != '.' && !isNaN(document.getElementById('text2').textContent[document.getElementById('text2').textContent.length - 1])) {
+        document.getElementById('text2').textContent += '.';
+        document.getElementById('textInput').textContent += '.';
+    }
 }
